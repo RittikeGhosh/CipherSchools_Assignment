0d29ac28ea5dc88a0eba9a7bc2de39931e28e67e
Rittike Ghosh <xyz@mail.com>
2021-02-17 01:52:29 +0530

    day2

--- Recursion and Backtracking/GenerateBalancedParanthesis.java
+++ Recursion and Backtracking/GenerateBalancedParanthesis.java
@@ -1,3 +1,20 @@
+import java.util.Scanner;
 public class GenerateBalancedParanthesis {
-    
+    static void genBalancedParenthesis(int n, String s, int openPar) {
+        // base case
+        if (openPar > n) return;
+        if (n == 0) {
+            System.out.println(s);
+            return;
+        }
+
+        genBalancedParenthesis(n - 1, s + "(", openPar + 1);
+        if (openPar > 0)
+            genBalancedParenthesis(n - 1, s + ")", openPar - 1);
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        genBalancedParenthesis(2 * n, "", 0);
+    }
 }
--- Recursion and Backtracking/SubsetSum.java
+++ Recursion and Backtracking/SubsetSum.java
@@ -1,3 +1,26 @@
+import java.util.Scanner;
 public class SubsetSum {
-    
+    static void genSubsetForSum(int arr[], int n, int k, String res) {
+        // base case
+        if (k == 0) {
+            StringBuilder new_s = new StringBuilder();
+            System.out.println(new_s.append(res.trim()).reverse());
+            return;
+        } 
+        if (n == 0) return;
+
+        genSubsetForSum(arr, n - 1, k, res);
+        if (k - arr[n - 1] >= 0)
+            genSubsetForSum(arr, n - 1, k - arr[n - 1], res + arr[n - 1] + " ");
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = in.nextInt();
+        int k = in.nextInt();
+
+        genSubsetForSum(arr, n, k, "");
+    }
 }
